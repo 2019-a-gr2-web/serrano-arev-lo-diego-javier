64a7c2159f4c9df5f252322989fbc4c4228fa024
jukishio <jukishio@hotmail.com>
2019-04-30 12:52:46 -0500

    calculadora cookie

--- deberes/01-calculadora/src/app.controller.ts
+++ deberes/01-calculadora/src/app.controller.ts
@@ -1,6 +1,7 @@
 import {Controller, Get, Post, HttpCode,Headers,Body,Response, Request, Delete,Put, Query} from '@nestjs/common';
 import { AppService } from './app.service';
 
+
 @Controller('/01calculadora')
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -16,52 +17,134 @@ export class AppController {
   //Método GET - SUMA
   @Get('/suma')
   @HttpCode(200)
-  sumaJPZ(@Headers() headers, @Request() request, @Response() response) {
-    if (headers.numero && headers.numero2) {
-      const cookie = request.cookies;
-      console.log('Headers: ', headers);
-      const numA = Number(headers.numero);
-      const numB = Number(headers.numero2);
-      const resp = numA + numB;
-      response.cookie('user', 'jukishio +');
-      return response.send({'La suma es: ': resp.toString(), 'user': cookie.user});
-    }else{
-      return response.status(200).send({mensaje: 'Error no hay valores para sumar', error: 200})
+  suma(@Headers() headers, @Request() request, @Response() response) {
+
+    const cookie = request.cookies;
+    const cookieSegura = request.signedCookies;
+    const numA = Number(headers.numero);
+    const numB = Number(headers.numero2);
+    const resp = numA + numB;
+
+    if(!cookieSegura.intentos){
+      response.cookie('intentos',100,{signed:true});
     }
+    console.log('Headers: ', headers);
+
+    if(!cookieSegura.nombreUsuario) {
+      response.cookie('user', 'jukishio');
+    }
+    const numIntentos = cookieSegura.intentos - resp;
+
+    if(numIntentos <=0){
+      const respuesta = {
+        resultado: `Resultado: ${resp}`,
+        user: `user: ${cookie.nombreUsuario}`,
+        mensaje: "SE HAN HAGOTADO LOS TOKENS"
+      };
+      response.send(respuesta);
+    }else{
+      const respuesta = {
+        resultado: resp,
+        user: `user: ${cookie.nombreUsuario}`,
+      };
+      if(cookieSegura.intentos){
+        response.cookie('Intentos Disponibles',numIntentos,{signed:true});
+      }
+      return response.send(respuesta);
+    }
+
   }
 
   //Método POST - RESTA
   @Post('/resta')
   @HttpCode(201)
-  restaJPZ(@Body() parametros, @Response() resp, @Request() request ){
+  resta(@Body() parametros, @Response() response, @Request() request ){
     console.log(parametros);
-    if(parametros.numero && parametros.numero2){
-      const cookie = request.cookies;
-      const numA = Number(parametros.numero);
-      const numB = Number(parametros.numero2);
-      const numResta = numA-numB;
-      resp.cookie('user', 'jukishio -');
-      return resp.send({'La resta es: ': numResta.toString(), 'user': cookie.user});
 
-    }else{
-      return resp.status(400).send("DEBE DE INGRESAR NUMEROS");
+    const cookie = request.cookies;
+    const cookieSegura = request.signedCookies;
+    const numA = Number(parametros.numero);
+    const numB = Number(parametros.numero2);
+    const numResta = numA-numB;
+
+    if(!cookieSegura.intentos){
+      response.cookie('intentos',100,{signed:true});
     }
+
+
+    if(!cookieSegura.nombreUsuario) {
+      response.cookie('user', 'jukishio');
+    }
+
+    const numIntentos = cookieSegura.intentos - numResta;
+
+
+
+
+
+    if(numIntentos <=0){
+      const respuesta = {
+        resultado: numResta,
+        user: `user:: ${cookie.nombreUsuario}`,
+        mensaje: "SE HAN HAGOTADO LOS TOKENS"
+      };
+      response.send(respuesta);
+    }else{
+      const respuesta = {
+        resultado: numResta,
+        user: `user: ${cookie.nombreUsuario}`,
+      };
+      if(cookieSegura.intentos){
+        response.cookie('Intentos Disponibles',numIntentos,{signed:true});
+      }
+      return response.send(respuesta);
+    }
+
+
 
   }
 
   //Metodo PUT - MULTIPLICACION
   @Put('/multiplicacion')
   @HttpCode(202)
-  multipJPZ(@Query() parametros, @Response() resp, @Request() request){
-    if(parametros.numero && parametros.numero2){
-      const cookie = request.cookies;
-      const numA = Number(parametros.numero);
-      const numB = Number(parametros.numero2);
-      const numMult = numA*numB;
-      resp.cookie('user', 'jukishio *');
-      return resp.send({'La multiplicacion es: ': numMult.toString(), 'user': cookie.user});
+  multiplicacion(@Query() parametros, @Response() response, @Request() request){
+
+    const cookie = request.cookies;
+    const cookieSegura = request.signedCookies;
+    const numA = Number(parametros.numero);
+    const numB = Number(parametros.numero2);
+    const numMult = numA*numB;
+
+    if(!cookieSegura.intentos){
+      response.cookie('intentos','100',{signed:true});
+    }
+
+
+    if(!cookieSegura.nombreUsuario) {
+      response.cookie('user', 'jukishio');
+    }
+
+    const numIntentos = cookieSegura.intentos - numMult;
+
+
+
+
+    if(numIntentos <=0){
+      const respuesta = {
+        resultado: numMult,
+        user: `user: ${cookie.nombreUsuario}`,
+        mensaje: "SE HAN HAGOTADO LOS TOKENS"
+      };
+      response.send(respuesta);
     }else{
-      return resp.status(400).send("DEBE DE INGRESAR NUMEROS EN QUERY");
+      const respuesta = {
+        resultado: numMult,
+        user: `user: ${cookie.nombreUsuario}`,
+      };
+      if(cookieSegura.intentos){
+        response.cookie('Intentos Disponibles',numIntentos,{signed:true});
+      }
+      return response.send(respuesta);
     }
 
   }
@@ -70,21 +153,47 @@ export class AppController {
   //Metodo DELETE - DIVISION
   @Delete('/division')
   @HttpCode(203)
-  diviJPZ(@Headers() paramHeader, @Body() paramBody, @Response() resp, @Request() request){
-    if(paramHeader.numero && paramBody.numero2) {
-      const cookie = request.cookies;
-      const numA = Number(paramHeader.numero);
-      const numB = Number(paramBody.numero2);
-      if (numB == 0) {
-        return resp.send('EL NUMERO DEBE DE SER DISTINTO DE 0')
+  division(@Headers() paramHeader, @Body() paramBody, @Response() response, @Request() request){
 
-      } else {
-        const div = numA / numB;
-        resp.cookie('user', 'jukishio /');
-        return resp.send({'La division es: ': div.toString(), 'user': cookie.user});
+    const cookie = request.cookies;
+    const cookieSegura = request.signedCookies;
+    const numA = Number(paramHeader.numero);
+    const numB = Number(paramBody.numero2);
+
+    if (numB == 0) {
+      return response.send('EL NUMERO DEBE DE SER DISTINTO DE 0')
+
+    } else {
+      const numDiv = numA / numB;
+      if(!cookieSegura.intentos){
+        response.cookie('intentos','100',{signed:true});
       }
-    }else{
-      return resp.status(400).send("DEBE DE INGRESAR NUMEROS EN QUERY");
+
+
+      if(!cookieSegura.nombreUsuario) {
+        response.cookie('user', 'jukishio');
+      }
+
+      const numIntentos = cookieSegura.intentos - numDiv;
+
+      if(numIntentos <=0){
+        const respuesta = {
+          resultado: numDiv,
+          user: `Usuario: ${cookie.nombreUsuario}`,
+          mensaje: "SE HAN HAGOTADO LOS TOKENS"
+        };
+        response.send(respuesta);
+      }else{
+        const respuesta = {
+          resultado: numDiv,
+          user: `user: ${cookie.nombreUsuario}`,
+        };
+        if(cookieSegura.intentos){
+          response.cookie('Intentos Disponibles',numIntentos,{signed:true});
+        }
+        return response.send(respuesta);
+      }
     }
+
   }
 }
